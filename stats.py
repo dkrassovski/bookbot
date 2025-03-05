8c2a5b13059114c82bc1d2cb3180a7b7a580005a
Deekay <krassovski.d@gmail.com>
2025-03-05 14:50:41 -0500

    updated code

--- stats.py
+++ stats.py
@@ -4,9 +4,7 @@ def get_book_text (path):
 def number_of_words():
     book_text = get_book_text("books/frankenstein.txt")
     book_text = book_text.split()
-    num_words = 0
-    for i in range(len(book_text)):
-        num_words = i + 1
+    num_words = len(book_text)
     print (f"{num_words} words found in the document")
 
 def count ():
@@ -14,10 +12,24 @@ def count ():
     book_text = book_text.lower()
     letter_count = {}
     for letter in book_text:
-        if letter in letter_count:
-            letter_count[letter] += 1
-        else:
-            letter_count[letter] = 1
-    print (letter_count) 
-number_of_words()
-count()
+        if letter.isalpha():
+            if letter in letter_count:
+                letter_count[letter] += 1
+            else:
+                letter_count[letter] = 1
+    return letter_count
+def sorted_letters(letter_count):
+    sorted_data = sorted([{"character": char, "count": count} for char, count in letter_count.items()], key=lambda x: x["count"], reverse=True)
+    return sorted_data
+def print_report():
+    print ("============ BOOKBOT ============")
+    print ("Analyzing book found at books/frankenstein.txt...")
+    print ("----------- Word Count ----------")
+    number_of_words()
+    print ("--------- Character Count -------")
+    letter_count = count()
+    sorted_data = sorted_letters(letter_count)
+    for item in sorted_data:
+        print(f"{item['character']}: {item['count']}")
+    print ("============= END ===============")
+print_report()
